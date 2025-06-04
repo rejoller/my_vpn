e0e794df6b76a2d438e60d78b692414d10d20aad
rejoller <130197750+rejoller@users.noreply.github.com>
2025-06-05 05:10:33 +0700

    сделана разбивка по файлам, обновлен requrements.txt и добавлен сервисный файл

---
+++ bot/handlers/on_click.py
@@ -0,0 +1,140 @@
+
+from aiogram.types import CallbackQuery
+
+from aiogram.types import LabeledPrice
+
+
+from aiogram_dialog import DialogManager
+
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, update, func
+from sqlalchemy.dialects.postgresql import insert
+
+import ctypes
+import uuid
+from database.models import User, Subscription, Server
+from database.engine import session_maker
+
+from typing import Optional
+import logging
+from datetime import datetime as dt
+
+from icecream import ic
+from datetime import timedelta
+
+from config import GO_FILE_PATH, API_ADDRESS, API_PORT, PBK, SHORT_VPN_ID
+
+
+
+
+
+
+
+async def balance_btn(callback: CallbackQuery, dialog_manager: DialogManager, data: None):
+    async with session_maker() as session:
+        # ic(callback, dialog_manager, data)
+        ic(data.find('location'))
+        ic(data.dialog_data)
+        query = select(User.balance_xtr).where(User.tg_user_id == int(callback.from_user.id))
+        response = await session.execute(query)
+        result = response.one()
+        await callback.answer(f'Ваш баланс {result[0]} tg stars ⭐️', show_alert=True)
+        
+        
+        
+async def status_btn(callback: CallbackQuery, dialog_manager: DialogManager, data: None):
+    async with session_maker() as session:
+
+        query = select(func.max(Subscription.end_date)).where(Subscription.tg_user_id == int(callback.from_user.id))
+        response = await session.execute(query)
+        result = response.one()
+        if result[0] is None:
+            await callback.answer('У вас нет подписки 🤨', show_alert=True)
+            return
+        result = dt.strftime(result[0], "%d.%m.%Y")
+
+        await callback.answer(f'Ваша подписка  действует до {result}', show_alert=True)
+        
+        
+
+        
+         
+    
+async def pay_btn(callback: CallbackQuery, session: AsyncSession, dialog_manager: DialogManager, data: Optional[dict] = None):
+    prices = [LabeledPrice(label="XTR", amount=1)]
+    await callback.message.answer_invoice(
+            title="Пополнить счет с помощью telegram stars",
+            description="Пополнить на 1 telegram stars",
+            payload="100_stars",
+            provider_token="",
+            currency="XTR",
+            prices=prices,
+            start_parameter="start_parameter_here"
+        )
+    
+    
+async def buy_btn(callback: CallbackQuery, dialog_manager: DialogManager, data):
+    async with session_maker() as session:
+        query = select(User.balance_xtr).where(User.tg_user_id == int(callback.from_user.id))
+        response = await session.execute(query)
+        result = response.one()
+        if result[0] < 1:
+            await callback.answer(f'Ваш баланс {result[0]} звезд😳 пополните счет', show_alert=True)
+            return
+        
+        email = str(callback.from_user.id)
+        email = email + "22"
+        vpn_uuid = str(uuid.uuid4())
+        query_balance_upd = update(User).where(User.tg_user_id == int(callback.from_user.id)).values(balance_xtr = User.balance_xtr - 1, vpn_uuid=vpn_uuid)
+        try:
+            address = f"{API_ADDRESS}:{API_PORT}"
+            lib = ctypes.CDLL(GO_FILE_PATH)
+            lib.InitClient.argtypes = [ctypes.c_char_p]
+            lib.InitClient(address.encode('utf-8'))
+            lib.addUser.argtypes = [ctypes.c_char_p, ctypes.c_char_p]
+            lib.addUser.restype = ctypes.c_char_p
+            lib.addUser(email.encode('utf-8'), vpn_uuid.encode('utf-8'))
+            
+            subscription_upd_query = insert(Subscription).values(tg_user_id = callback.from_user.id,
+                                            start_date = dt.now(),
+                                            end_date = dt.now() + timedelta(days=30))
+
+            
+            await session.execute(query_balance_upd)
+            await session.execute(query)
+            await session.execute(subscription_upd_query)
+            await session.commit()
+            
+            await callback.message.answer('Покупка совершена, спасибо☺️')
+            
+        except Exception as e:
+            logging.error(f"Ошибка при добавлении подписки: {e}")
+            await session.rollback()
+            await callback.message.answer('Ошибка при покупке, попробуйте позже')
+            
+            
+            
+async def config_btn(callback: CallbackQuery, dialog_manager: DialogManager, *args, **kwargs):
+    async with session_maker() as session:
+        short_name_location = str.split(callback.data, ':')[1]
+        query = select(User.vpn_uuid).where(User.tg_user_id == int(callback.from_user.id))
+        response = await session.execute(query)
+        result = response.one()
+        
+        if not result:
+            await callback.answer('Сначала нужно купить подписку', show_alert=True)
+            return
+        vpn_uuid = result[0]
+        
+        query = select(Server.address).where(Server.location == short_name_location)
+        response = await session.execute(query)
+        address = response.one()[0]
+        
+
+    url = f"""vless://{vpn_uuid}@{address}:443?security=reality&encryption=none&pbk={PBK}&headerType=none&fp=chrome&type=tcp&flow=xtls-rprx-vision&sni=www.microsoft.com&sid={SHORT_VPN_ID}#CreengeVPNBot"""
+    await callback.message.answer(text= f"""
+                                  Ваш конфиг <pre>{url}</pre>\n\n<b>Как подключиться?</b>\n1. Скопируйте ключ.\n\
+2. Откройте Hiddify v2RayTun(или аналогичный клиент).\n\
+3. Нажмите ➕ (Добавить)\n4. 'Добавить из буфера обмена'.\n\
+5. Подключение завершено, пользуйтесь ❤️""", parse_mode='HTML')
--- bot/handlers/vpn.py
+++ bot/handlers/vpn.py
@@ -10,12 +10,10 @@ from aiogram_dialog import Dialog, DialogManager, StartMode, Window
 from aiogram_dialog.widgets.text import Const, Format
 from aiogram_dialog.widgets.kbd import Button, Cancel, Select, Group, Back
 
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, update
 from sqlalchemy.dialects.postgresql import insert
 
-import ctypes
-import uuid
+
 from database.models import User, Transaction, Subscription, Server
 from database.engine import session_maker
 
@@ -24,9 +22,9 @@ import logging
 from datetime import datetime as dt
 
 from icecream import ic
-from datetime import timedelta
 
-from config import GO_FILE_PATH, API_ADDRESS, API_PORT, PBK, SHORT_VPN_ID
+
+from handlers.on_click import pay_btn, buy_btn, balance_btn, status_btn, config_btn
 
 
 
@@ -60,12 +58,15 @@ async def get_locations(dialog_manager: DialogManager,**kwargs):
 
 
 
+
+
+
 @router.message(F.successful_payment) 
 @router.message(CommandStart(), F.chat.type == "private")
 async def handle_subscribe(message: Message, dialog_manager: DialogManager, new_user: bool):
 
     if message.successful_payment:
-        ic(message.successful_payment)
+
         amount = message.successful_payment.total_amount
         
         async with session_maker() as session_:
@@ -119,100 +120,6 @@ async def handle_subscribe(message: Message, dialog_manager: DialogManager, new_
     
     
     
-    
-async def balance_btn(callback: CallbackQuery, dialog_manager: DialogManager, data: None):
-    async with session_maker() as session:
-        # ic(callback, dialog_manager, data)
-        ic(data.find('location'))
-        ic(data.dialog_data)
-        query = select(User.balance_xtr).where(User.tg_user_id == int(callback.from_user.id))
-        response = await session.execute(query)
-        result = response.one()
-        await callback.answer(f'Ваш баланс {result[0]} tg stars ⭐️', show_alert=True)
-        
-         
-    
-async def pay_btn(callback: CallbackQuery, session: AsyncSession, dialog_manager: DialogManager, data: Optional[dict] = None):
-    prices = [LabeledPrice(label="XTR", amount=1)]
-    await callback.message.answer_invoice(
-            title="Пополнить счет с помощью telegram stars",
-            description="Пополнить на 1 telegram stars",
-            payload="100_stars",
-            provider_token="",
-            currency="XTR",
-            prices=prices,
-            start_parameter="start_parameter_here"
-        )
-    
-    
-async def buy_btn(callback: CallbackQuery, dialog_manager: DialogManager, data):
-    async with session_maker() as session:
-        query = select(User.balance_xtr).where(User.tg_user_id == int(callback.from_user.id))
-        response = await session.execute(query)
-        result = response.one()
-        if result[0] < 1:
-            await callback.answer(f'Ваш баланс {result[0]} звезд😳 пополните счет', show_alert=True)
-            return
-        
-        email = str(callback.from_user.id)
-        email = email + "22"
-        vpn_uuid = str(uuid.uuid4())
-        query_balance_upd = update(User).where(User.tg_user_id == int(callback.from_user.id)).values(balance_xtr = User.balance_xtr - 1, vpn_uuid=vpn_uuid)
-        try:
-            address = f"{API_ADDRESS}:{API_PORT}"
-            lib = ctypes.CDLL(GO_FILE_PATH)
-            lib.InitClient.argtypes = [ctypes.c_char_p]
-            lib.InitClient(address.encode('utf-8'))
-            lib.addUser.argtypes = [ctypes.c_char_p, ctypes.c_char_p]
-            lib.addUser.restype = ctypes.c_char_p
-            lib.addUser(email.encode('utf-8'), vpn_uuid.encode('utf-8'))
-            
-            subscription_upd_query = insert(Subscription).values(tg_user_id = callback.from_user.id,
-                                            start_date = dt.now(),
-                                            end_date = dt.now() + timedelta(days=30))
-
-            
-            await session.execute(query_balance_upd)
-            await session.execute(query)
-            await session.execute(subscription_upd_query)
-            await session.commit()
-            
-            await callback.message.answer('Покупка совершена, спасибо☺️')
-            
-        except Exception as e:
-            logging.error(f"Ошибка при добавлении подписки: {e}")
-            await session.rollback()
-            await callback.message.answer('Ошибка при покупке, попробуйте позже')
-            
-
-
-                
-        
-async def get_config(callback: CallbackQuery, dialog_manager: DialogManager, *args, **kwargs):
-    async with session_maker() as session:
-        short_name_location = str.split(callback.data, ':')[1]
-        query = select(User.vpn_uuid).where(User.tg_user_id == int(callback.from_user.id))
-        response = await session.execute(query)
-        result = response.one()
-        
-        if not result:
-            await callback.answer('Сначала нужно купить подписку', show_alert=True)
-            return
-        vpn_uuid = result[0]
-        
-        query = select(Server.address).where(Server.location == short_name_location)
-        response = await session.execute(query)
-        address = response.one()
-        
-
-    url = f"""vless://{vpn_uuid}@{address}:443?security=reality&encryption=none&pbk={PBK}&headerType=none&fp=chrome&type=tcp&flow=xtls-rprx-vision&sni=www.microsoft.com&sid={SHORT_VPN_ID}#CreengeVPNBot"""
-    await callback.message.answer(text= f'Ваш конфиг <pre>{url}</pre>', parse_mode='HTML')
-    
-        
-
-        
-    
-    
 @router.pre_checkout_query()
 async def process_pre_checkout_query(pre_checkout_query: PreCheckoutQuery, bot: Bot):
     await bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True)
@@ -224,7 +131,7 @@ sel_locations = Select(
     id="main",
     item_id_getter=lambda x: x,
     items="location",
-    on_click=get_config,
+    on_click=config_btn,
 )
 
     
@@ -235,6 +142,7 @@ dialog = Dialog(
         Format("получить доступ к ВПН"),
         Button(Const("💰 Пополнить счет через telegram stars"), id="pay", on_click=pay_btn),
         Button(Const("💳 Узнать баланс"), id= "bal", on_click=balance_btn),
+        Button(Const("🐺 Статус подписки"), id= "status", on_click=status_btn),
         Button(Const("🔐 Оплатить подписку со своего счета"), id="buy", on_click=buy_btn),
         Button(Const("🌍 Доступные сервера"), id = "serv", on_click=available_locations),
         state=MySG.window1,
